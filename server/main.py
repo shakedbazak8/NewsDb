3725022fbb096072b53acd1761fc0ffd77786352
user <user@mail.com>
2024-11-04 14:40:47 +0200

    api config.

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+import config
+
 if __name__ == '__main__':
-    uvicorn.run(app="news_controller:app", reload=True)
+    uvicorn.run(app="news_controller:app", reload=True, host=config.HOST, port=config.PORT)
