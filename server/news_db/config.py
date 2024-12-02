0c6dbf69edc609248e1d537cf9e09c8b99dbd12b
user <user@mail.com>
2024-12-02 10:21:21 +0200

    remote oracle instead of docker.

--- server/news_db/config.py
+++ server/news_db/config.py
@@ -1,9 +1,10 @@
+import os
 HOST = "localhost"
 PORT = 8003
 
 ORACLE_USER = 'system'
-ORACLE_PASS = 'a'
-ORACLE_DSN = 'localhost:1521/FREE'
+ORACLE_PASS = os.getenv("ORACLE_PASSWORD")
+ORACLE_DSN = '185.126.81.91:1521/FREE'
 
 
 BASE_DIR = './storage'
